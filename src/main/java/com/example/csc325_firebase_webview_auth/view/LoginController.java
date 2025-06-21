8806c3b2d16bdea4f6ca86483225c50161f820c7
DanS-89 <stevens.j.daniel@gmail.com>
2025-06-21 18:33:47 -0400

    Commit - added loginController

---
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/LoginController.java
@@ -0,0 +1,79 @@
+package com.example.csc325_firebase_webview_auth.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+
+public class LoginController {
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton, signUpButton;
+
+    @FXML
+    private Label statusLabel;
+
+    private static final String API_KEY = "AIzaSyBV-Csx6BSIUBGWiU2ar3OltY_1yH3sU9o";
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        String email = emailTextField.getText();
+        String password = passwordField.getText();
+
+        if(email.isEmpty() || password.isEmpty()){
+            statusLabel.setText("Please enter a valid email address and password");
+            return;
+        }
+
+        try {
+            URL url = new URL("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + API_KEY);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            String jsonInput = String.format("{\"email\":\"%s\",\"password\":\"%s\",\"returnSecureToken\":true}",email,password);
+
+         try (OutputStream os = conn.getOutputStream()) {
+             byte[] input = jsonInput.getBytes(StandardCharsets.UTF_8);
+             os.write(input, 0, input.length);
+         }
+
+         InputStream inputStream = conn.getResponseCode() == 200 ? conn.getInputStream() : conn.getErrorStream();
+         String response = new BufferedReader(new InputStreamReader(inputStream)).lines().reduce("", String::concat);
+         JSONObject jsonObject = new JSONObject(response);
+
+         if(conn.getResponseCode() == 200) {
+             String idToken = jsonObject.getString("idToken");
+             statusLabel.setText("Successfully logged in");
+             App.setRoot("/files/AccessFBView.fxml");
+         } else {
+             String error = jsonObject.getJSONObject("error").getString("message");
+
+             statusLabel.setText("Login failed: " + error);
+         }
+        } catch (Exception e) {
+            statusLabel.setText("Login failed: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    private void handleSignUp(ActionEvent event) throws IOException {
+        App.setRoot("/files/SignUp.fxml");
+    }
+}
